abf623e687a2420aa7ee77763c431ede0b1002bd
Alberto <murillojaenesalberto@gmail.com>
2021-02-11 20:42:23 +0100

    Heroku

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -30,12 +30,12 @@ BASEURL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decidedb',
-        'USER': 'decide2',
-	'PASSWORD': 'decide',
-        'HOST': 'localhost',
+        'NAME': 'd3vluoj2jnujlr',
+        'USER': 'uqgnhnylevyoyz',
+        'PASSWORD':
+        'c5671fcf406df953b82c838e805c7904a6636869c7675f7cba98001fd8f99192',
+        'HOST': 'ec2-54-145-249-177.compute-1.amazonaws.com',
         'PORT': '5432',
-    }
 }
 
 # number of bits for the key, all auths should use the same number of bits
