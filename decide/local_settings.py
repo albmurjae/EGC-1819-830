99fa43317642b24637fe3cb9f01e8c96c1cddd56
Alberto <murillojaenesalberto@gmail.com>
2021-02-11 23:32:25 +0100

    Heroku

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -13,30 +13,31 @@ MODULES = [
     'voting',
 ]
 
+BASEURL = 'https://pruebaegcalberto.herokuapp.com'
+
 APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL,
 }
 
-BASEURL = 'http://localhost:8000'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decidedb',
-        'USER': 'decide2',
-	'PASSWORD': 'decide',
-        'HOST': 'localhost',
+        'NAME': 'dee895if7dcqtj',
+        'USER': 'uqgnhnylevyoyz',
+        'PASSWORD':
+        'c5671fcf406df953b82c838e805c7904a6636869c7675f7cba98001fd8f99192',
+        'HOST': 'ec2-54-145-249-177.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
