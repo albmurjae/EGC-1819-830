fc6304f04dd30217594e6a81035faa7660891ecf
Alberto <murillojaenesalberto@gmail.com>
2021-02-11 15:10:01 +0100

    travis

--- decide/local_settings.travis.py
+++ decide/local_settings.travis.py
@@ -30,8 +30,8 @@ BASEURL = 'http://localhost:8000'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decidedb',
-        'USER': 'decide2',
+        'NAME': 'decide',
+        'USER': 'decide',
 	'PASSWORD': 'decide',
         'HOST': 'localhost',
         'PORT': '5432',
